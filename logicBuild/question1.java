973731c88c967065492fd26fc0c6b37fe90f17e7
raju sharma <rajusharmadata123@gmail.com>
2024-11-02 23:21:06 +0530

    add

--- logicBuild/question1.java
+++ logicBuild/question1.java
@@ -15,26 +15,57 @@ Output: false
 Explanation: "raceacar" is not a palindrome
  */
 
-  // Time complexity - o(n)
-  // Space complexity -o(1)
+// Time complexity - o(n)
+// Space complexity -o(1)
 public class question1 {
-public static boolean palindrome(String s){
-    // IN THIS LINE REPLACE ALL THE NONALPHABETICUL REMOVE IN YOUR STRING 
-    String result = s.replaceAll("[^a-zA-Z0-9]", "");
-    System.out.println(result);
-    // CONVERT TO LOWER CASE CHARECTOR 
-    result = result.toLowerCase();
-            //CHECK THE GIVEN STRING IS PALINDROME OR NO 
-         for(int i = 0;i<= ((result.length())/2);i++){
-            if(result.charAt(i)!=result.charAt(result.length()-1-i)){
-                return false;
-            }
-         }
-         return true;
+    /*
 
+    public static boolean palindrome(String s){
+     * // IN THIS LINE REPLACE ALL THE NONALPHABETICUL REMOVE IN YOUR STRING
+     * String result = s.replaceAll("[^a-zA-Z0-9]", "");
+     * System.out.println(result);
+     * // CONVERT TO LOWER CASE CHARECTOR
+     * result = result.toLowerCase();
+     * //CHECK THE GIVEN STRING IS PALINDROME OR NO
+     * for(int i = 0;i<= ((result.length())/2);i++){
+     * if(result.charAt(i)!=result.charAt(result.length()-1-i)){
+     * return false;
+     * }
+     * }
+     * return true;
+     * 
 }
-    public static void main(String [] args){
-        String s = "0P";
-        System.out.println(palindrome(s));
+     */
+
+
+    public  static boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while (start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "noon";
+        // System.out.println(palindrome(s));
+        System.out.println(isPalindrome(s));
     }
 }
