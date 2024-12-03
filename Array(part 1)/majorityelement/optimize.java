cf642fb5a7dfb018d72771d4dd80b375b4ad76e4
rajusharmadata <rajusharmadata123@gmail.com>
2024-12-03 16:53:58 +0530

    add

--- Array(part 1)/majorityelement/optimize.java
+++ Array(part 1)/majorityelement/optimize.java
@@ -1,35 +1,36 @@
 public class optimize {
-    public static int majorityelment(int arr[]){
-        int n =arr.length;
+    public static int majorityelment(int arr[]) {
+        int n = arr.length;
         int el = 0;
         int freq = 0;
         int ans = 0;
-        for(int i = 0;i<n;i++){
-            if(freq==0){
+        for (int i = 0; i < n; i++) {
+            if (freq == 0) {
                 freq++;
                 el = arr[i];
-            }else if(el==arr[i]){
+            } else if (el == arr[i]) {
                 freq++;
-            }else{
+            } else {
                 freq--;
             }
         }
 
         int count = 0;
-        for(int j = 0;j<n;j++){
-            if(arr[j]==el){
+        for (int j = 0; j < n; j++) {
+            if (arr[j] == el) {
                 count++;
-            
+
             }
-            if(count>n/2){
+            if (count > n / 2) {
                 ans = arr[j];
             }
         }
 
         return ans;
     }
+
     public static void main(String[] args) {
-        int arr[] = {3,2,3};
-        System.out.println(majorityelment(arr)) ;
+        int arr[] = { 3, 2, 3 };
+        System.out.println(majorityelment(arr));
     }
 }
